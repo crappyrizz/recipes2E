fca6ef4ad164be0533eaf0d8680ab1ec60bf2b36
crappyrizz <155568733+crappyrizz@users.noreply.github.com>
2024-05-21 13:27:14 +0300

    Initial commit

---
+++ calculatesum.php
@@ -0,0 +1,11 @@
+<?php
+
+$num1 = 1;
+$num2 = 1;
+
+
+$sum = $num1 + $num2;
+
+
+echo "The sum of $num1 and $num2 is: $sum";
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="ütf-8">
+        <meta name="viewport" content="width-device-width, initial-scale=1.0">
+        <title>form</title>
+
+    </head>
+    <body>
+        <form action="">
+            <label for="fno">first name</label>
+            <input type="number"id="fno"class="nameInput"placeholder="first no">
+            <label for="lno">last name</label>
+            <input type="number"id="lno"class="nameInput"placeholder="second no">
+            <input type="submit"value="add">
+        </form>
+    </body>
+</html>
